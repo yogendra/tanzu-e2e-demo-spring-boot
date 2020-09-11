5e17a387650b94c0a55da80cfe21ec46174bd89b
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2020-09-11 14:23:32 +0800

    init

---
+++ src/main/java/com/example/demo/UIController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Greg Turnquist
+ */
+// tag::code[]
+@Controller // <1>
+public class UIController {
+
+  @RequestMapping(value = "/") // <2>
+  public String index() {
+    return "index"; // <3>
+  }
+
+}
